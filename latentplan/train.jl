e5631e26c34c9a6ae401d2f13c642a9d6f7462b2
eneserciyes <m.eneserciyes@gmail.com>
2023-02-04 09:54:39 +0000

    remove checkpoint loading

--- latentplan/train.jl
+++ latentplan/train.jl
@@ -196,7 +196,7 @@ n_epochs = Int(floor((1e6 / length(dataset)) * args["n_epochs_ref"]))
 save_freq = Int(floor(n_epochs / args["n_saves"]))
 # wandb.init(project="latentplan.jl", config=args, tags=[args["exp_name"], args["tag"]])
 # load from checkpoint
-model = Knet.load(joinpath(args["savepath"], "state_0.jld2"), "model")
+# model = Knet.load(joinpath(args["savepath"], "state_0.jld2"), "model")
 for epoch in 1:n_epochs
     logfile = open(joinpath(args["savepath"], "log2.txt"), "a")
     
@@ -211,10 +211,10 @@ for epoch in 1:n_epochs
         total_loss = @diff losssum(model(X, Y, mask, terminal))
         println("Loss #", it, ": ", value(total_loss))
         println(logfile, "Loss #", it, ": ", value(total_loss))
-        prev_model = deepcopy(model)
+        # prev_model = deepcopy(model)
         if isnan(value(total_loss))
             println(logfile, "NaN loss!!")
-            Knet.save(joinpath(args["savepath"], "nan_model_2.jld2"),"prev_model", prev_model, "model", model, "batch", batch)
+            # Knet.save(joinpath(args["savepath"], "nan_model_2.jld2"),"prev_model", prev_model, "model", model, "batch", batch)
             return
         end
         for p in paramlist(model)
